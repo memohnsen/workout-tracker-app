51fcc4d161073d83ba4ac5e86e32099c867d7a35
Maddisen Mohnsen <memohnsen@gmail.com>
2025-08-24 13:19:50 -0400

    scroll view

--- strong-clone/Views/Home/HomeView.swift
+++ strong-clone/Views/Home/HomeView.swift
@@ -11,55 +11,57 @@ struct HomeView: View {
     @State private var showAlert = false
     
     var body: some View {
-        VStack(alignment: .center, spacing: 30) {
-            HStack {
-                Button("Start An Empty Workout") {
-                    showAlert = true
-                }.frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
-            }.padding(.horizontal, 20)
-
-            
-            HStack {
-                Text("Templates").font(.title2)
-                Spacer()
+        ScrollView {
+            VStack(alignment: .center, spacing: 30) {
+                HStack {
+                    Button("Start An Empty Workout") {
+                        showAlert = true
+                    }.frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }.padding(.horizontal, 20)
                 
-                Button("+ Template") {
+                
+                HStack {
+                    Text("Templates").font(.title2)
+                    Spacer()
                     
-                }.buttonStyle(.borderedProminent)
+                    Button("+ Template") {
+                        
+                    }.buttonStyle(.borderedProminent)
+                    
+                    Button(action: {}) {
+                        Image(systemName: "folder")
+                    }.buttonStyle(.borderedProminent)
+                    
+                    Button(action: {}) {
+                        Image(systemName: "ellipsis")
+                    }.buttonStyle(.borderedProminent)
+                }
                 
-                Button(action: {}) {
+                HStack {
                     Image(systemName: "folder")
-                }.buttonStyle(.borderedProminent)
+                    Text("My Templates (2)")
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Image(systemName: "ellipsis")
+                    }.buttonStyle(.borderedProminent)
+                }
                 
-                Button(action: {}) {
-                    Image(systemName: "ellipsis")
-                }.buttonStyle(.borderedProminent)
-            }
-            
-            HStack {
-                Image(systemName: "folder")
-                Text("My Templates (2)")
-                Spacer()
+                LazyVGrid(columns: [
+                    GridItem(.flexible()),
+                    GridItem(.flexible())
+                ], spacing: 15) {
+                    WorkoutCard(title: "Workout 1")
+                    WorkoutCard(title: "Workout 2")
+                    WorkoutCard(title: "Workout 3")
+                    WorkoutCard(title: "Workout 4")
+                }
                 
-                Button(action: {}) {
-                    Image(systemName: "ellipsis")
-                }.buttonStyle(.borderedProminent)
             }
-            
-            LazyVGrid(columns: [
-                GridItem(.flexible()),
-                GridItem(.flexible())
-            ], spacing: 15) {
-                WorkoutCard(title: "Workout 1")
-                WorkoutCard(title: "Workout 2")
-                WorkoutCard(title: "Workout 3")
-                WorkoutCard(title: "Workout 4")
-            }
-            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         
@@ -69,7 +71,7 @@ struct HomeView: View {
         
         
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(red: 186/255, green: 164/255, blue: 235/255))
+        .background(Color.gray.opacity(0.1))
     }
 }
 
